ede0f3fe447542e24a886958d9633448fdd1a57e
Dmitry Turin <turin.dmytro@gmail.com>
2015-08-20 19:20:42 +0300

    Add one more test project

---
+++ Foo.Tests/FTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Foo.Tests
+{
+    using System;
+
+    using TestCoverageProject;
+
+    using Xunit;
+
+    public class FTest
+    {
+        [Fact]
+        public void FTest_Ctor()
+        {
+            Assert.Throws<ArgumentException>(() => new Foo("test"));
+        }
+    }
+}
