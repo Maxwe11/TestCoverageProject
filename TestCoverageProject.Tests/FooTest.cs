9a7edeb56d0329439b49ce71450ed889d1e4485e
Dmitry Turin <turin.dmytro@gmail.com>
2015-08-20 16:30:04 +0300

    initial commit

---
+++ TestCoverageProject.Tests/FooTest.cs
@@ -0,0 +1,13 @@
+﻿namespace TestCoverageProject.Tests
+{
+    using Xunit;
+
+    public class FooTest
+    {
+        [Fact]
+        public void Ctor_Success()
+        {
+            new Foo("test");
+        }
+    }
+}
---
+++ TestCoverageProject/Foo.cs
@@ -0,0 +1,19 @@
+﻿namespace TestCoverageProject
+{
+    using System;
+
+    public class Foo
+    {
+        public Foo(string bar)
+        {
+            if (string.IsNullOrEmpty(bar))
+            {
+                throw new ArgumentNullException(nameof(bar));
+            }
+
+            Bar = bar;
+        }
+
+        public string Bar { get; }
+    }
+}
