b46bd17c941e5606439b695ded1467b4cfcdead4
Dmitry Turin <turin.dmytro@gmail.com>
2015-08-24 12:18:41 +0300

    Test SerialPort on CI

--- TestCoverageProject.Tests/FooTest.cs
+++ TestCoverageProject.Tests/FooTest.cs
@@ -1,6 +1,8 @@
 ﻿namespace TestCoverageProject.Tests
 {
     using System;
+    using System.IO.Ports;
+    using System.Threading;
 
     using Xunit;
 
@@ -16,6 +18,28 @@
         public void Ctor_Fail()
         {
             Assert.Throws<ArgumentException>(() => new Foo(string.Empty));
+        }
+
+        [Fact]
+        public static void SerialPort_Test()
+        {
+            const string expected = "FooBar";
+            string actual = null;
+            var port = new SerialPort("COM1");
+            var port2 = new SerialPort("COM1");
+            var mres = new ManualResetEventSlim();
+            port2.DataReceived += (sender, e) =>
+            {
+                actual = port2.ReadExisting();
+                mres.Set();
+            };
+
+            port.Open();
+            port2.Open();
+            port.Write(expected);
+
+            Assert.True(mres.Wait(TimeSpan.FromSeconds(2)), "Timedout");
+            Assert.Equal(expected, actual);
         }
     }
 }
