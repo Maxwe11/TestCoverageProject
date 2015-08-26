dfffe63f59dd5834b5d77b099951609d522ee342
Dmitry Turin <turin.dmytro@gmail.com>
2015-08-26 17:07:21 +0300

    Remove SerialPort test

--- TestCoverageProject.Tests/FooTest.cs
+++ TestCoverageProject.Tests/FooTest.cs
@@ -1,9 +1,6 @@
 ﻿namespace TestCoverageProject.Tests
 {
     using System;
-    using System.IO.Ports;
-    using System.Threading;
-
     using Xunit;
 
     public class FooTest
@@ -18,28 +15,6 @@
         public void Ctor_Fail()
         {
             Assert.Throws<ArgumentException>(() => new Foo(string.Empty));
-        }
-
-        [Fact]
-        public static void SerialPort_Test()
-        {
-            const string expected = "FooBar";
-            string actual = null;
-            var port = new SerialPort("COM1");
-            var port2 = new SerialPort("COM1");
-            var mres = new ManualResetEventSlim();
-            port2.DataReceived += (sender, e) =>
-            {
-                actual = port2.ReadExisting();
-                mres.Set();
-            };
-
-            port.Open();
-            port2.Open();
-            port.Write(expected);
-
-            Assert.True(mres.Wait(TimeSpan.FromSeconds(2)), "Timedout");
-            Assert.Equal(expected, actual);
         }
     }
 }
